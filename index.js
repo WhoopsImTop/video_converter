cdfdbc456bd0d102b6dd64cb713c48fd2a90b152
Elias Englen <eliasenglen2@gmail.com>
2023-09-08 14:29:50 +0200

    add new

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const ffmpeg = require("fluent-ffmpeg");
 const request = require("request");
 /* ffmpeg.setFfmpegPath("C:/ffmpeg/bin/ffmpeg.exe"); */
 ffmpeg.setFfmpegPath("/usr/bin/ffmpeg");
+const axios = require("axios");
+require("dotenv").config();
 
 var bodyParser = require("body-parser");
 const multer = require("multer");
@@ -18,7 +20,71 @@ app.use(cors());
 // for parsing application/json
 app.use(bodyParser.json());
 
-// for parsing application/xwww-
+let lastRequestTime = null;
+
+// Middleware, um die Zeit der letzten Anfrage in eine Datei zu schreiben
+app.use((req, res, next) => {
+  const currentTime = new Date();
+  const formattedTime = currentTime.toISOString();
+
+  // Schreibe die Zeit der letzten Anfrage in die Datei (z.B. lastRequestTime.txt)
+  fs.writeFile("lastRequestTime.txt", formattedTime, (err) => {
+    if (err) {
+      console.error(
+        "Fehler beim Schreiben der letzten Anfragezeit in die Datei:",
+        err
+      );
+    } else {
+      console.log(
+        "Zeit der letzten Anfrage in die Datei geschrieben:",
+        formattedTime
+      );
+    }
+  });
+
+  // Aktualisiere lastRequestTime
+  lastRequestTime = currentTime;
+
+  // Rufe die nächste Middleware oder Route auf
+  next();
+});
+
+// Funktion zur Überprüfung der letzten Anfragezeit und Ausführung des PUT-Requests
+function checkLastRequestTime() {
+  const currentTime = new Date();
+  if (lastRequestTime) {
+    const timeDifference = currentTime - lastRequestTime;
+    const minutesSinceLastRequest = timeDifference / (1000 * 60);
+
+    if (minutesSinceLastRequest >= 1) {
+      // Hier kannst du den PUT-Request ausführen, z.B. mit axios
+      const config = {
+        url: `https://scp-api.strato.de/v1/servers/${STRATO_SERVER_ID}/status`,
+        method: "put",
+        headers: {
+          "X-TOKEN": STRATO_API_TOKEN,
+          "Content-Type": "application/json",
+        },
+        data: {
+          action: "POWER_OFF",
+          method: "HARDWARE",
+        },
+      };
+      axios
+        .request(config)
+        .then((response) => {
+          console.log("PUT-Request erfolgreich ausgeführt:", response.data);
+        })
+        .catch((error) => {
+          console.error("Fehler beim Ausführen des PUT-Requests:", error);
+        });
+    }
+  }
+}
+
+// Überprüfe die letzte Anfragezeit alle Minute
+setInterval(checkLastRequestTime, 60 * 1000); // 60 * 1000 Millisekunden entsprechen einer Minute
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post("/convert", upload.single("file"), (req, res) => {
